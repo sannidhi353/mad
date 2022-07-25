f90631ed727c2238fa36518c929c43845c5d62fe
Sannidhi Rao <68779176+sannidhi353@users.noreply.github.com>
2022-07-25 19:24:44 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,38 @@
+package com.example.parsingxmljson;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.parsingxmljson.R;
+import com.example.parsingxmljson.ViewDataActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button parseXmlBtn,parseJsonBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        parseXmlBtn=(Button)findViewById(R.id.main_parse_xml_data);
+        parseXmlBtn.setOnClickListener(this);
+        parseJsonBtn=(Button) findViewById(R.id.main_parse_json_data);
+        parseJsonBtn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view.equals(parseXmlBtn))
+        {
+            Intent intent=new Intent(this, ViewDataActivity.class);
+            intent.putExtra("datatype","xml");
+            startActivity(intent);
+        }
+        else if(view.equals(parseJsonBtn))
+        {
+            Intent intent=new Intent(this,ViewDataActivity.class);
+            intent.putExtra("datatype","json");
+            startActivity(intent);
+        }
+    }
+}
